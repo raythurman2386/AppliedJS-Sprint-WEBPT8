e0b156378e2fc4f22bf737c98b9026f1ea0b0307
raythurman2386 <raymondthurman5@gmail.com>
2019-08-18 19:19:39 -0400

    initial axios and tab function set up

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+		.get(`https://lambda-times-backend.herokuapp.com/topics`)
+		.then((res) => {
+      console.log(res.data.topics);
+      const data = res.data.topis;
+      data.forEach(element => {
+        Tabs(element);
+      });
+      
+		})
+		.catch((error) => {
+			console.log(error);
+    });
+    
+
+function Tabs(tabData) {
+  const tab = document.createElement('div');
+  tab.classList.add('tab');
+  tab.textContent = tabData;
+
+  return tab;
+}
